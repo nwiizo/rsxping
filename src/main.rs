86caa20ccc428772f51d8371c6781d443fa13063
smotouchi <shuya-motouchi@gmo.jp>
2018-08-29 11:54:01 +0900

    入力値のチェック

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate futures;
+extern crate tokio;
+
+extern crate tokio_ping;
+
+use futures::{Future, Stream};
+
+fn usage() {
+        println!("rsxping <ipaddress>")
+}
+
+fn main() {
+    let addr = std::env::args().nth(1) {
+        Some(addr) => addr,
+        None => {
+            usage();
+            return;
+        }
+    };
+
+    let pinger = tokio_ping::Pinger::new();
+    let stream = pinger.and_then(move |pinger| Ok(pinger.chain(addr).stream()));
+    let future = stream.and_then(|stream| {
+        stream.take(3).for_each(|mb_time| {
+            match mb_time {
+                Some(time) => println!("time={}", time),
+                None => println!("timeout"),
+            }
+            Ok(())
+        })
+    });
+
+    tokio::run(future.map_err(|err| {
+        eprintln!("Error: {}", err)
+    }))
+}
